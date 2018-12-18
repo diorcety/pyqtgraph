b84a7762c928ef5fab2e7cd4cd0f5a7640ff989e
Yann Diorcet <yann.diorcet@xenocs.com>
2018-12-18 12:03:24 +0100

    Support logMode in ErrorBarItem

--- pyqtgraph/graphicsItems/ErrorBarItem.py
+++ pyqtgraph/graphicsItems/ErrorBarItem.py
@@ -2,6 +2,7 @@ from ..Qt import QtGui, QtCore
 from .GraphicsObject import GraphicsObject
 from .. import getConfigOption
 from .. import functions as fn
+import numpy as np
 
 __all__ = ['ErrorBarItem']
 
@@ -21,7 +22,8 @@ class ErrorBarItem(GraphicsObject):
             left=None,
             right=None,
             beam=None,
-            pen=None
+            pen=None,
+            logMode= [False, False],
         )
         self.setData(**opts)
 
@@ -53,7 +55,16 @@ class ErrorBarItem(GraphicsObject):
     def setOpts(self, **opts):
         # for backward compatibility
         self.setData(**opts)
-        
+
+
+    def getDrawValue(self, data, horizontal):
+        log = (horizontal and self.opts['logMode'][0]) or (not horizontal and self.opts['logMode'][1])
+        if log:
+            with np.errstate(invalid='ignore', divide='ignore'):
+                return np.log10(data)
+        else:
+            return data
+
     def drawPath(self):
         p = QtGui.QPainterPath()
         
@@ -62,7 +73,6 @@ class ErrorBarItem(GraphicsObject):
             return
         
         beam = self.opts['beam']
-        
         
         height, top, bottom = self.opts['height'], self.opts['top'], self.opts['bottom']
         if height is not None or top is not None or bottom is not None:
@@ -79,22 +89,32 @@ class ErrorBarItem(GraphicsObject):
                     y2 = y
                 else:
                     y2 = y + top
-            
-            for i in range(len(x)):
-                p.moveTo(x[i], y1[i])
-                p.lineTo(x[i], y2[i])
+
+            rx = self.getDrawValue(x, True)
+            ry1 = self.getDrawValue(y1, False)
+            ry2 = self.getDrawValue(y2, False)
+
+            #Filter
+            is_nan = np.logical_or(np.isnan(rx), np.logical_or(np.isnan(ry1), np.isnan(ry2)))
+            rx = rx[np.logical_not(is_nan)]
+            ry1 = ry1[np.logical_not(is_nan)]
+            ry2 = ry2[np.logical_not(is_nan)]
+
+            for i in range(len(rx)):
+                p.moveTo(rx[i], ry1[i])
+                p.lineTo(rx[i], ry2[i])
                 
             if beam is not None and beam > 0:
-                x1 = x - beam/2.
-                x2 = x + beam/2.
+                x1 = rx - beam/2.
+                x2 = rx + beam/2.
                 if height is not None or top is not None:
-                    for i in range(len(x)):
-                        p.moveTo(x1[i], y2[i])
-                        p.lineTo(x2[i], y2[i])
+                    for i in range(len(rx)):
+                        p.moveTo(x1[i], ry2[i])
+                        p.lineTo(x2[i], ry2[i])
                 if height is not None or bottom is not None:
-                    for i in range(len(x)):
-                        p.moveTo(x1[i], y1[i])
-                        p.lineTo(x2[i], y1[i])
+                    for i in range(len(rx)):
+                        p.moveTo(x1[i], ry1[i])
+                        p.lineTo(x2[i], ry1[i])
         
         width, right, left = self.opts['width'], self.opts['right'], self.opts['left']
         if width is not None or right is not None or left is not None:
@@ -111,26 +131,44 @@ class ErrorBarItem(GraphicsObject):
                     x2 = x
                 else:
                     x2 = x + right
-            
-            for i in range(len(x)):
-                p.moveTo(x1[i], y[i])
-                p.lineTo(x2[i], y[i])
+
+            rx1 = self.getDrawValue(x1, True)
+            rx2 = self.getDrawValue(x2, True)
+            ry = self.getDrawValue(y, False)
+
+            #Filter
+            is_nan = np.logical_or(np.isnan(rx1), np.logical_or(np.isnan(rx2), np.isnan(ry)))
+            rx1 = rx1[np.logical_not(is_nan)]
+            rx2 = rx2[np.logical_not(is_nan)]
+            ry = ry[np.logical_not(is_nan)]
+
+            for i in range(len(rx)):
+                p.moveTo(rx1[i], ry[i])
+                p.lineTo(rx2[i], ry[i])
                 
             if beam is not None and beam > 0:
-                y1 = y - beam/2.
-                y2 = y + beam/2.
+                y1 = ry - beam/2.
+                y2 = ry + beam/2.
                 if width is not None or right is not None:
-                    for i in range(len(x)):
-                        p.moveTo(x2[i], y1[i])
-                        p.lineTo(x2[i], y2[i])
+                    for i in range(len(rx)):
+                        p.moveTo(rx2[i], y1[i])
+                        p.lineTo(rx2[i], y2[i])
                 if width is not None or left is not None:
-                    for i in range(len(x)):
-                        p.moveTo(x1[i], y1[i])
-                        p.lineTo(x1[i], y2[i])
+                    for i in range(len(rx)):
+                        p.moveTo(rx1[i], y1[i])
+                        p.lineTo(rx1[i], y2[i])
                     
         self.path = p
         self.prepareGeometryChange()
-        
+
+    def setLogMode(self, xMode, yMode):
+        if self.opts['logMode'] == [xMode, yMode]:
+            return
+        self.opts['logMode'] = [xMode, yMode]
+        self.path = None
+        self.update()
+        self.prepareGeometryChange()
+        self.informViewBoundsChanged()
         
     def paint(self, p, *args):
         if self.path is None:
